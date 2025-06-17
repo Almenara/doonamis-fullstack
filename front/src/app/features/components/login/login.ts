355dd633106b7bf3be045a856e54ae9b3b8b0298
Alberto Almenara <alberto.almenara@gmail.com>
2025-06-17 17:26:32 +0200

    formulario funcionando

--- front/src/app/features/components/login/login.ts
+++ front/src/app/features/components/login/login.ts
@@ -1,42 +1,64 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+
+import { UserLoginResponse } from '../../../models/auth';
 import { AuthService } from './../../../services/auth.service';
 import { AlertService } from '../../../services/alert.service';
-import { User } from './../../../models/user';
-import { UserLoginResponse } from '../../../models/auth';
-import { CommonModule } from '@angular/common';
-import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 
 })
 export class Login {
 
-  private authService: AuthService = inject(AuthService);
-  private alertService: AlertService = inject(AlertService);
-  private userService: UserService = inject(UserService);
-  public users: User[] = [];
+private authService: AuthService    = inject(AuthService);
+  private alertService: AlertService  = inject(AlertService);
 
-  public login(): void {
-    this.authService.login({'email': 'admin@admin.com', 'password': '12345'}).subscribe({
+  private fb = inject(FormBuilder);
+  public loginForm = this.fb.group({
+    email:    ['admin@admin.com', Validators.required],
+    password: ['12345', Validators.required]
+  });
+
+  public onSubmit(): void {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.login(
+      this.loginForm.value as { email: string; password: string }
+    );
+    this.loginForm.reset();
+  }
+
+
+  private login(credentials: { email: string; password: string }): void {
+    this.authService.login(credentials).subscribe({
       next: (response: UserLoginResponse) => {
         if(response.data.message){
           this.alertService.success(response.data.message, {autoClose: true, keepAfterRouteChange: true});
         }
       },
       error: () => {
-        this.alertService.error('Login failed', {autoClose: true, keepAfterRouteChange: false});
+        this.alertService.error('Algo ha ido mal', {autoClose: true, keepAfterRouteChange: false});
       }
     });
   }
+
+
+
   public logout(): void {
     this.authService.logout();
   }
 
-  public getUsers(){
+  /*public getUsers(){
     this.userService.getUsers().subscribe({
       next: (response: User[]) => {
         this.users = response;
@@ -46,6 +68,6 @@ export class Login {
         //TODO: handle error
       }
     });
-  }
+  }*/
 
 }
