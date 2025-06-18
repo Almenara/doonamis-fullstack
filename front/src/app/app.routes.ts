da313b1497dea44c340d00df820e61cd481bddbf
Alberto Almenara <alberto.almenara@gmail.com>
2025-06-18 11:18:25 +0200

    rutas protegidas

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,20 +1,23 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/components/home/home.component';
 import { Login } from './features/components/login/login';
+import { AuthGuard, LoginGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
 
     {
         path: '',
-        redirectTo: 'login',
         pathMatch: 'full',
+        redirectTo: 'login',
     },
     {
-        path: 'home',
         component: Home,
+        path: 'home',
+        canActivate: [AuthGuard],
     },
     {
         path: 'login',
         component: Login,
+        canActivate: [LoginGuard]        
     },
 ];
